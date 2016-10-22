aa165ff9cb3f912ae2d494a5f6fde81ac7b7b3ce
vjay <vjay@vjay-VirtualBox>
2016-10-22 17:19:25 +0200

    Configuration page prototype working

--- src/main/java/de/equalIT/jiraExchangeConnector/adminUI/AdminServlet.java
+++ src/main/java/de/equalIT/jiraExchangeConnector/adminUI/AdminServlet.java
@@ -1,7 +1,10 @@
 package de.equalIT.jiraExchangeConnector.adminUI;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,9 +16,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.atlassian.plugin.spring.scanner.annotation.component.JiraComponent;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.pluginsettings.PluginSettings;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.templaterenderer.TemplateRenderer;
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
 
+/**
+ * For the later configuration in the browser.
+ * 
+ * @author vjay@gmx.net
+ *
+ */
 @SuppressWarnings("serial")
 @JiraComponent
 public class AdminServlet extends HttpServlet {
@@ -26,44 +39,57 @@ public class AdminServlet extends HttpServlet {
 
 	protected TemplateRenderer renderer;
 
-	// public void AuswertungJira(@ComponentImport TemplateRenderer
-	// templateRenderer) {
-	// this.renderer = templateRenderer;
-	// }
-
-	// public AdminServlet() {
-	// super();
-	// System.out.println("YAYAYAYAYAY");
-	// System.out.println("YAYAYAYAYAY");
-	// System.out.println("YAYAYAYAYAY");
-	// System.out.println("YAYAYAYAYAY");
-	// System.out.println("YAYAYAYAYAY");
-	// System.out.println("YAYAYAYAYAY");
-	// }
+	protected PluginSettingsFactory pluginSettingsFactory;
 
 	@Autowired
-	public AdminServlet(@ComponentImport TemplateRenderer renderer, @ComponentImport UserManager userManager, @ComponentImport LoginUriProvider loginUriProvider) {
+	public AdminServlet(@ComponentImport PluginSettingsFactory pluginSettingsFactory, @ComponentImport TemplateRenderer renderer, @ComponentImport UserManager userManager, @ComponentImport LoginUriProvider loginUriProvider) {
 		this.userManager = userManager;
 		this.loginUriProvider = loginUriProvider;
 		this.renderer = renderer;
+		this.pluginSettingsFactory = pluginSettingsFactory;
 	}
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		//		System.out.println("REQUEST");
-		//		System.out.println("REQUEST");
-		//		System.out.println("loginUriProvider is " + loginUriProvider);
-		//		System.out.println("userManager is " + userManager);
-		//		System.out.println("renderer is " + renderer);
+		System.out.println("GOT " + request.getRequestURI());
 
 		String username = userManager.getRemoteUsername(request);
 		if (username == null || !userManager.isSystemAdmin(username)) {
 			redirectToLogin(request, response);
 			return;
 		}
+		if (Boolean.parseBoolean(request.getParameter("config"))) {
+			PluginSettings settings = pluginSettingsFactory.createGlobalSettings();
+			Map<String, Object> result = Maps.newHashMap();
+			result.put("name", settings.get("Config.name"));
+			result.put("time", settings.get("Config.time"));
+
+			String resultString = new Gson().toJson(result);
+
+			System.out.println("Sending:" + resultString);
+			response.setContentType("text/json;charset=utf-8");
+			response.getWriter().print(resultString);
+			response.getWriter().flush();
+			return;
+		}
 
 		response.setContentType("text/html;charset=utf-8");
 		renderer.render("admin.vm", response.getWriter());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost " + request.getRequestURI());
+
+		try (Reader reader = request.getReader();) {
+			Map<String, Object> config = new Gson().fromJson(reader, HashMap.class);
+			PluginSettings settings = pluginSettingsFactory.createGlobalSettings();
+			settings.put("Config.name", config.get("name"));
+			settings.put("Config.time", config.get("time"));
+		}
+		response.getWriter().print("OK");
+		response.getWriter().flush();
 	}
 
 	private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
---
+++ src/main/resources/admin.js
@@ -0,0 +1,39 @@
+(function ($) { // this closure helps us keep our variables to ourselves.
+// This pattern is known as an "iife" - immediately invoked function expression
+ 
+    // form the URL
+    var url = /*AJS.contextPath() +*/ "/jira/plugins/servlet/jiraExchangeConnector?config=true";
+ 
+    // wait for the DOM (i.e., document "skeleton") to load. This likely isn't necessary for the current case,
+    // but may be helpful for AJAX that provides secondary content.
+    $(document).ready(function() {
+        // request the config information from the server
+        $.ajax({
+            url: url,
+            dataType: "json"
+        }).done(function(config) { // when the configuration is returned...
+            // ...populate the form.
+            $("#name").val(config.name);
+            $("#time").val(config.time);
+            
+            
+            function updateConfig() {
+            	  AJS.$.ajax({
+            	    url: url,
+            	    type: "POST",
+            	    contentType: "application/json",
+            	    data: '{ "name": "' + AJS.$("#name").attr("value") + '", "time": "' +  AJS.$("#time").attr("value") + '" }',
+            	    processData: false
+            	  });
+            	  console.log('{ "name": "' + AJS.$("#name").attr("value") + '", "time": "' +  AJS.$("#time").attr("value") + '" }');
+            	}
+            
+            AJS.$("#admin").submit(function(e) {
+                e.preventDefault();
+                updateConfig();
+            });
+            
+        });
+    });
+
+})(AJS.$ || jQuery);
